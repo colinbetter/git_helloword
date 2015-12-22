7e4cb4bba142ebf5743f873e92fdfb801a6595d5
h11689 <h11689@h11689-dmz-c01>
2015-12-22 13:10:37 +0800

    i change the file

--- src/com/hx/HelloGit.java
+++ src/com/hx/HelloGit.java
@@ -1,7 +1,10 @@
 package com.hx;
 
+import java.util.Date;
+
 public class HelloGit {
 	public static void main(String args[]){
 		System.out.println("Hello Git");
+		System.out.println("i change the file"+new Date());
 	}
 }
