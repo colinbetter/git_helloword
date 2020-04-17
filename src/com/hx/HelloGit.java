7d52cdce7964a52924d0d5dfd360d6c356763b09
colin better <hxinglearn@163.com>
2020-04-17 23:30:08 +0800

    789

--- src/com/hx/HelloGit.java
+++ src/com/hx/HelloGit.java
@@ -12,5 +12,9 @@ public class HelloGit {
 		System.out.println("Hello Git");
 		System.out.println("Hello Git");
 
+		System.out.println("i change the file at Tue Dec 22 14:09:58 CST 2015");
+		System.out.println("i change the file at Tue Dec 22 14:09:58 CST 2015");
+		System.out.println("i change the file at Tue Dec 22 14:09:58 CST 2015");
+		System.out.println("i change the file at Tue Dec 22 14:09:58 CST 2015");
 	}
 }
